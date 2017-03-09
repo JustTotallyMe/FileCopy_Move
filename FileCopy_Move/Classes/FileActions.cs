85d71169585a6f9e025b32ca0293515c4a6a53ad
Andreas <andy.leonardo.ab@gmail.com>
2017-03-09 10:18:50 +0100

    Added new Project to GH

---
+++ FileCopy_Move/Classes/FileActions.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+
+namespace FileCopy_Move.Classes
+{
+    class FileActions
+    {
+        string _destFolder, _destPathComp, _drivePath, _driveName;
+        DirectoryInfo _sourceDirInfo;
+
+        public FileActions()
+        {
+            _destFolder = ConfigurationManager.AppSettings["DestFolder"];
+            _driveName = ConfigurationManager.AppSettings["DriveName"];
+            _sourceDirInfo = new DirectoryInfo(ConfigurationManager.AppSettings["SourceFolder"]);
+        }
+
+        #region Public Methods
+        public string RunMoveWorkflow()
+        {
+            try
+            {
+                var list = GetCreationDates_NoDoubles();
+                CreateFoldersFromList(list);
+                Copy_MoveFiles(true);
+
+                return "DONE";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public string RunCopyWorkflow()
+        {
+            try
+            {
+                var list = GetCreationDates_NoDoubles();
+                CreateFoldersFromList(list);
+                Copy_MoveFiles(false);
+
+                return "DONE";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public void SetDestPathComp(bool isDriveChecked)
+        {
+            if (isDriveChecked)
+            {
+                _destPathComp = _drivePath + _destFolder;
+            }
+            else
+            {
+                _destPathComp = _destFolder;
+            }
+        }
+
+        public bool checkForSpecificDrive()
+        {
+            DriveInfo[] ad = DriveInfo.GetDrives();
+            bool returnvalue = false;
+            foreach (DriveInfo d in ad)
+            {
+                if (d.IsReady == true)
+                {
+                    if (d.VolumeLabel != null)
+                    {
+                        if (d.VolumeLabel == _driveName)
+                        {
+                            returnvalue = true;
+                            _drivePath = d.Name;
+                            break;
+                        }
+                    }
+                }
+            }
+            return returnvalue;
+        }
+        #endregion
+
+        #region Private Methods
+        void Copy_MoveFiles(bool isMove)
+        {
+            foreach (FileInfo item in _sourceDirInfo.GetFiles())
+            {
+                if (isMove)
+                {
+                    File.Move(item.FullName, _destPathComp + "\\" + item.LastWriteTime.ToString("dd_MM_yyyy") + "\\" + item.Name);
+                }
+                else
+                {
+                    File.Copy(item.FullName, _destPathComp + "\\" + item.LastWriteTime.ToString("dd_MM_yyyy") + "\\" + item.Name);
+                }
+                Console.Write("=");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Files moved to " + _destPathComp);
+        }
+
+        void CreateFoldersFromList(List<string> testing)
+        {
+            foreach (var item in testing)
+            {
+                if (!Directory.Exists($"{_destPathComp}\\{item}"))
+                {
+                    Directory.CreateDirectory(_destPathComp + "\\" + item);
+                }
+            }
+        }
+
+        List<string> GetCreationDates_NoDoubles()
+        {
+            List<string> testing = new List<string>();
+            foreach (FileInfo item in _sourceDirInfo.GetFiles())
+            {
+                if (!testing.Contains(item.LastWriteTime.ToString("dd_MM_yyyy")))
+                {
+                    testing.Add(item.LastWriteTime.ToString("dd_MM_yyyy"));
+                }
+            }
+            return testing;
+        }
+        #endregion
+    }
+}
---
+++ FileCopy_Move/Program.cs
@@ -0,0 +1,36 @@
+﻿using FileCopy_Move.Classes;
+using System;
+using System.Configuration;
+
+namespace FileCopy_Move
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileActions FA = new FileActions();
+
+            if (args != null)
+            {
+                if (args[0] == "DriveCheck")
+                {
+                    if (FA.checkForSpecificDrive())
+                    {
+                        FA.SetDestPathComp(true);
+                    }
+                }
+            }
+            else
+            {
+                FA.SetDestPathComp(false);
+            }
+
+            switch (ConfigurationManager.AppSettings["Usage"].ToLower())
+            {
+                case "move": Console.WriteLine(FA.RunMoveWorkflow()); break;
+                case "copy": Console.WriteLine(FA.RunCopyWorkflow()); break;
+                default: Console.WriteLine("Function not implemented. Please change \"Usage\"."); break;
+            }
+        }
+    }
+}
